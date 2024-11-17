415d3153292f6fc801e33786d92d284f8221a935
Shruti18bhatia <121923282+Shruti18bhatia@users.noreply.github.com>
2024-11-17 15:56:54 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+myName = "Shruti"
+myAge = 20
+myState = 'UP'
+myMaritalStatus = false
+myJob = null 
+myFuture = undefined
+
+console.table([typeof(myName),typeof(myAge),typeof(myState),typeof(myMaritalStatus),typeof(myJob),typeof(myFuture)])
