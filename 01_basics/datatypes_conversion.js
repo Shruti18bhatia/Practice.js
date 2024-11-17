4cf41806599698e95a53ee6a347cd887ecba20df
Shruti18bhatia <121923282+Shruti18bhatia@users.noreply.github.com>
2024-11-17 17:01:49 +0000

    conversion of dt

---
+++ 01_basics/datatypes_conversion.js
@@ -0,0 +1,14 @@
+let num = null //88
+let number =  //"hyy" //false //undefined //null //"88aa"
+console.log(typeof(num));
+console.log(typeof(number));
+let numb = Number(number)
+console.log(typeof(numb));
+console.log(numb);
+let isit = 5
+let wasit = 0
+console.log(typeof isit);
+let st = Boolean(isit)
+let str = Boolean(wasit)
+console.log(st);
+console.log(str);
