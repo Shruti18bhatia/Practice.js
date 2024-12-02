2608ed8a6931324d11349f9ef0dbba5c13cfb193
Shruti18bhatia <121923282+Shruti18bhatia@users.noreply.github.com>
2024-12-02 16:48:40 +0000

    string,math,num

---
+++ 01_basics/numbers.js
@@ -0,0 +1,28 @@
+// const score = 300
+// console.log(score)
+// const fscore = new Number(200)
+// console.log(fscore)
+// console.log(fscore.toString().length);
+// console.log(fscore.toFixed(3));
+
+// aNum = 1343.43123
+// console.log(aNum.toFixed(2))
+// console.log(aNum.toPrecision(3))
+// console.log(aNum.toPrecision(7))
+
+// console.log(aNum.toLocaleString('en-IN'))
+
+//          MATHS                //
+// console.log(Math);
+// console.log(Math.abs(-4.5));
+// console.log(Math.round(34.32));
+// console.log(Math.ceil(34.32));
+// console.log(Math.floor(34.32));
+// console.log(Math.min(3,4,3,2));
+
+console.log((Math.random()*100) + 1)
+console.log((Math.floor(Math.random()*100) + 1))
+
+const min = 90
+const max = 100
+console.log((Math.floor(Math.random()*(max - min + 1)) + min))
---
+++ 01_basics/string.js
@@ -0,0 +1,16 @@
+myName = "Shruti"
+myAge = 20
+eg = "Hyy , Myself Shruti From My-Own-World"
+console.log(`My name is ${myName} and my age is ${myAge}`);
+console.log(myName[3]);
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(4));
+console.log((myName.indexOf('u')));
+console.log(myName.substring(0,3))
+console.log(myName.slice(-4,4))
+
+console.log(eg.replace('Shruti' , '   '))
+console.log(eg.includes('Shruti'))
+
+console.log(eg.split('-'))
