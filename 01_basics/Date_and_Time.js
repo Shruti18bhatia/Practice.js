400cf869bb590f38d4ae08e2fbc34ae124a0fca8
Shruti18bhatia <121923282+Shruti18bhatia@users.noreply.github.com>
2024-12-06 14:41:27 +0000

    date and time

---
+++ 01_basics/Date_and_Time.js
@@ -0,0 +1,9 @@
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+let my_Date = new Date(2025,6,3)
+console.log(my_Date)
+
+console.log(Date.now().toLocaleString())
